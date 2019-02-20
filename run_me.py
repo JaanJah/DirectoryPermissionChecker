dd2485ffd31cbff0bf40a4384e8fc3f7437177bb
JaanJahimees <jaanjahilo@outlook.com>
2019-02-20 08:20:46 +0200

    Fixed whitespace problem in directory path.

--- run_me.py
+++ run_me.py
@@ -4,15 +4,16 @@ import subprocess
 path = input("Please enter directory path: ")
 print ("You have entered " + path + " as your directory path.\n")
 
+fixedPath =  '"' + path + '"'
 cwd = os.getcwd()
 completePath = cwd + "\dirPath.txt"
 fixPath = cwd.replace("\\","/")
 batFilePath = fixPath + "/dpc.bat"
 
 dirPathFile = open(completePath, "w")
-dirPathFile.write(path)
-dirPathFile.close();
+dirPathFile.write(fixedPath)
+dirPathFile.close()
 print ("Directory path set successfully\n")
 
 print ("Executing DirectoryPermissionChecker\n\n")
-os.system(batFilePath);
+os.system(batFilePath)
